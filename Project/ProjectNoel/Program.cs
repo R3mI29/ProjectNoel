b4b5048a83b67f394c4bc05886682d094d6cbec1
Remi MILLOT <161507446+R3mI29@users.noreply.github.com>
2025-11-10 23:56:22 +0100

    Add files via upload

---
+++ Project/ProjectNoel/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public class Param
+    {
+        
+    }
+    public static void InitParam()
+    {
+        Param Noel = new Param();
+    }
+
+
+
+    public static void Main()
+    {
+
+    }
+}
