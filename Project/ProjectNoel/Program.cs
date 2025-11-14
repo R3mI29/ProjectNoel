966beaa286cc707d59faa609d6afdfc90b0e9fad
Remi MILLOT <161507446+R3mI29@users.noreply.github.com>
2025-11-14 16:43:27 +0100

    Add comments for clarity in Program.cs
    Added comments to clarify the purpose and functionality of methods and classes.

--- Project/ProjectNoel/Program.cs
+++ Project/ProjectNoel/Program.cs
@@ -2,10 +2,22 @@ using System.Reflection.Metadata;
 
 public class Program
 {
+    //Auteur : Rémi
+    //Fonction/Class : ClearConsole
+    //Paramètres : Aucun
+    //Renvoie : Void
+    //Utilité : La fonction sert à nettoyer la console, pour rendre l'interaction de l'utilisateur avec cette dernioère plus facile et agréable.
     public static void ClearConsole ()
     {
         Console.Clear();
     }
+
+    //Auteur : Rémi
+    //Fonction/Class : DemandeInt
+    //Paramètres : Aucun
+    //Renvoie : Int
+    //Utilité : La fonction sert à verifier si ce que l'utilisateur à rentrer est bien un int, et si il est positif.
+    //          Si ce n'est pas le cas, la fonction lui envoie un message d'erreur et lui demande une nouvelle valeur.
     public static int DemandeInt()
     {
         bool condition = false;
@@ -27,6 +39,12 @@ public class Program
         ClearConsole();
         return Valeur;
     }
+
+    //Auteur : Rémi
+    //Fonction/Class : Param
+    //Paramètres : Aucun
+    //Renvoie : Rien
+    //Utilité : La class Param sert à demander à l'utilisateur de definir les valeurs qui vont servir le programme durant la suite de son fonctionement.
     public class Param
     {
         public int NBLutins { get; set; }
@@ -48,6 +66,9 @@ public class Program
             NBLettresParHeures = DemandeInt();
         }
     }
+
+
+
     public static void Main()
     {
         ClearConsole();
