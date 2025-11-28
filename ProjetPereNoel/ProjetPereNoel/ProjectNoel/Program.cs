0eedf5f87343060733f1119b893dce90aceb5b86
Remi MILLOT <161507446+R3mI29@users.noreply.github.com>
2025-11-28 18:05:18 +0100

    Enhance Program with new enums and Lettre attributes
    Added new enums for Continents and EtatTravail. Enhanced Lettre class to include Continent and Adresse attributes. Updated CreerLettre function to generate random addresses and continents.

--- ProjetPereNoel/ProjetPereNoel/ProjectNoel/Program.cs
+++ ProjetPereNoel/ProjetPereNoel/ProjectNoel/Program.cs
@@ -26,6 +26,30 @@ namespace ProjectNoel
 
         }
 
+
+        //Auteur : Rémi
+        //Type : Continent
+        //Ce type représente les différents continents que le père noël doit couvrir.
+        public enum Continents
+        {
+            Afrique,
+            AmeriqueDuNord,
+            AmeriqueDuSud,
+            Asie,
+            Europe,
+            Oceanie
+        }
+
+
+        //Auteur : Rémi
+        //Type : EtatTravail
+        //Ce type renseigne les status qu'un travailleur peut avoir dans cet algo.
+        public enum EtatTravail
+        {
+            Travail,
+            Attente
+        }
+
         //----------------------------------------------------------------------------------------------------------------------//
         //----------------------------------------------------------------------------------------------------------------------//
         //----------------------------------------------------Classes-----------------------------------------------------------//
@@ -34,8 +58,6 @@ namespace ProjectNoel
 
         //---------------------------------------------Classe Param---------------------------------------------//
         //Auteur : Rémi
-        //Paramètres : Aucun
-        //Renvoie : Rien
         //Utilité : La class Param sert à demander à l'utilisateur de definir les valeurs qui vont servir le programme durant la suite 
         // de son fonctionement.
         public class Param
@@ -229,32 +251,224 @@ namespace ProjectNoel
             {
                 this.Liste.Clear();//Vide la liste qui sert à représenter la pile
             }
+
+            //----Méthode Taille----//
+            //Auteur : Rémi 
+            //Description : Renvoie un int, qui est la taille de la pile
+            public int Taille()
+            {
+                int count = 0;
+                Node<T> node = this.Liste.Head;
+
+                while (node != null)
+                {
+                    count++;
+                    node = node.Next;
+                }
+                return count;
+            }
+
         }
+
+
         //---------------------------------------------Classe Lettre---------------------------------------------//        
-        //Auteur : Tancrède
+        //Auteur : Tancrède, Rémi
         //Description : classe qui représente une lettre avec trois attributs, le nom et le prénom de l'enfant et le jouet demandé par l'enfant
         public class Lettre
         {
             public string Nom {get; set;}
             public string Prenom {get; set;}
+            public Continents Continent {get; set;}
             public Jouet Jouet {get; set;}
+            public string Adresse {get; set;}
 
             //Constructeur
-            public Lettre( Jouet jouet, string nom, string prenom)
+            public Lettre( Jouet jouet, string nom, string prenom, Continents continent, string adresse)
             {
-                Nom = nom;
-                Prenom = prenom;
-
-                //On utilisera la fonction AgeToJouet pour ce paramètre
-                Jouet = jouet;
+                Nom = nom;                  //On utilisera la fonction CreeLettre pour ce paramètre
+                Prenom = prenom;            //On utilisera la fonction CreeLettre pour ce paramètre
+                Jouet = jouet;              //On utilisera la fonction AgeToJouet pour ce paramètre
+                Continent = continent;      //On utilisera la fonction CreeLettre pour ce paramètre
+                Adresse = adresse;          //On utilisera la fonction CreeLettre pour ce paramètre
             }
 
             public void Affiche()
             {
-                Console.WriteLine($"{Prenom} {Nom} a demandé un {Jouet} au Père Noël");
+                Console.WriteLine($"{Prenom} {Nom} du {Adresse} venant de d'{Continent} a demandé un {Jouet} au Père Noël");
             }
 
         }
+
+         //---------------------------------------------Classe Traineau---------------------------------------------//
+        // Auteur : Rémi
+        // Utilité : La classe Traineau sert à Initialiser les traineaux qui serviront pour la livraison des cadeaux
+        public class Traineau
+        {
+            public Continents Continent { get; set; }
+            public int CapaciteMax { get; set; }
+            public Pile<Lettre> PileCadeaux { get; set; }
+            public bool Parti { get; set; }
+            public int TempsAvantRetour { get; set; }
+
+            //Constructeur
+            public Traineau(int capacite, Continents continent)
+            {
+                this.CapaciteMax = capacite;
+                this.Continent = continent;
+                this.PileCadeaux = new Pile<Lettre>(); // Initialise la pile de cadeau
+            }
+
+            //Auteur : Rémi
+            //Fonction/Class : ChargeTraineau
+            //Paramètres : lettre (Lettre)
+            //Renvoie : void
+            //Utilité : La fonction sert à ajouter les cadeaux au traineaux.
+            public void ChargeTraineau(Lettre lettre)
+            {
+                PileCadeaux.Empile(lettre);
+            }
+
+            //Auteur : Rémi
+            //Fonction/Class : Plein
+            //Renvoie : bool
+            //Utilité : La fonction renvoie un bool qui nous dis si le traineau est plein
+            public bool Plein()
+            {
+                return PileCadeaux.Taille() >= CapaciteMax;
+            }
+
+            //Auteur : Rémi
+            //Fonction/Class : EnVoyages
+            //Renvoie : Void
+            //Utilité : La fonction sert à savoir si le traineau à fini la tourné, si oui alors la pile des cadeaux est vidée et parti = false, sinon alors on enlève 1 heure du temps avant son retour et ont dis qu'il est encore en livraison.
+            public void EnVoyage()
+            {
+                if (Parti == true && TempsAvantRetour <= 0)
+                {
+                    Parti = false;
+                    PileCadeaux.Vide();
+                }
+                else if (Parti == true)
+                {
+                    TempsAvantRetour--;
+                    Console.WriteLine("Encore en Livraison");
+                }
+            }
+
+            //Auteur : Rémi
+            //Fonction/Class : Depart
+            //Renvoie : Void
+            //Utilité : La fonction prépare les valeurs du traineau pour son départ.
+            public void Depart()
+            {
+                if (PileCadeaux.Taille() > 0)
+                {
+                    Parti = true;
+                    TempsAvantRetour = 6;
+                }
+            }
+        }   
+
+
+        //---------------------------------------------Classe Nain---------------------------------------------//
+        // Auteur : Rémi
+        // Utilité : La classe Nain sert à créer les nains, il sont ceux qui emballe les caadeau pour les donnés aux elfes.
+        public class Nain
+        {
+            public int ID {get; set;}
+            public string Nom {get; set;}
+            public EtatTravail Statut {get; set;}
+            public int TempRestant {get; set;}
+            public Lettre? LettreActuelle {get; set;}
+            
+            //Constructeur
+            public Nain(int id, string nom)
+            {
+                this.ID = id;
+                this.Nom = nom;
+                this.LettreActuelle = null;
+                this.TempRestant = 0;
+                this.Statut = EtatTravail.Attente;
+
+            }
+
+            //Auteur : Rémi
+            //Fonction/Class : DebutEmballage
+            //Paramètres : lettre (Lettre)
+            //Renvoie : Void
+            //Utilité : La fonction initialise les valeurs du nain pour qu'il puisse commencer à emballer.
+            public void DebutEmballage(Lettre lettre)
+            {
+                if(Statut == EtatTravail.Attente && lettre != null)
+                {
+                    LettreActuelle = lettre;
+                    TempRestant = 2;
+                    Statut = EtatTravail.Travail;
+                }
+            }
+
+            //Auteur : Rémi
+            //Fonction/Class : Emballage
+            //Renvoie : Lettre
+            //Utilité : La fonction renvoie les lettres fini et garde le nain occupé aussi longtemps que necessaire.
+            public Lettre Emballage()
+            {
+                if(Statut == EtatTravail.Travail && LettreActuelle != null)
+                {
+                    TempRestant--;
+                    if(TempRestant > 0)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        Lettre lettreFini = LettreActuelle;
+                        LettreActuelle = null;
+                        Statut = EtatTravail.Attente;
+
+                        return lettreFini;
+                    }
+                }
+                else if (Statut == EtatTravail.Attente)
+                {
+                    return null;
+                }
+                return null;
+            }
+        }
+
+
+        //---------------------------------------------Classe Elfe---------------------------------------------//
+        // Auteur : Rémi
+        // Utilité : La classe Elfe sert à créer les elfe qui vont servir à charger les traineaux. Chaque elfe à son continent.
+        public class Elfe
+        {
+            public Continents Continent {get; set;}
+            public int ID {get; set;}
+            public Traineau TraineauCont {get; set;}
+
+            //Constructeur
+            public Elfe(Continents continent, int id, int capaciteMax)
+            {
+                Continent = continent;
+                ID = id;
+                TraineauCont = new Traineau(capaciteMax, continent);
+            }
+
+            //Auteur : Rémi
+            //Fonction/Class : AjouteTraineau
+            //Paramètres : lettre (Lettre)
+            //Renvoie : Void
+            //Utilité : La fonction ajoutre au traineau le cadeau que l'elfe a avec lui.
+            public void AjouteTraineau(Lettre lettre)
+            {
+                TraineauCont.ChargeTraineau(lettre);
+            }
+        }
+
+
+
+
         //----------------------------------------------------------------------------------------------------------------------//
         //----------------------------------------------------------------------------------------------------------------------//
         //------------------------------------------------------Méthodes--------------------------------------------------------//
@@ -319,6 +533,23 @@ namespace ProjectNoel
             else{return Jouet.Ordinateur;}
         }
 
+        //---------------------------------------------Fonction RandomContinent---------------------------------------------// 
+        //Auteur : Rémi
+        //Fonction/Class : RandomContinent
+        //Paramètres : nbr (int)
+        //Renvoie : Continents
+        //Utilité : La fonction renvoie un continent en fonction du numero au hazard qui est en paramètre.
+        public static Continents RandomContinent(int nbr)
+        {
+            if(nbr < 0 || nbr > 5){throw new Exception("Le numéro n'est pas convenable");}
+            else if(nbr == 0){return Continents.Afrique;}
+            else if(nbr == 1){return Continents.AmeriqueDuNord;}
+            else if(nbr == 2){return Continents.AmeriqueDuSud;}
+            else if(nbr == 3){return Continents.Asie;}
+            else if(nbr == 4){return Continents.Europe;}
+            else{return Continents.Oceanie;}
+        }
+
         //---------------------------------------------Fonction CreerLettre---------------------------------------------//        
         //Auteur : Tancrède, Rémi
         //Description : Fonction qui génére une lettre aléatoire et l'ajoute dans la pile de lettre sur le bureau du Père Noël prise en paramètre
@@ -329,10 +560,14 @@ namespace ProjectNoel
             string[] ListePrenoms = {"Gabriel", "Léo", "Maël", "Noah", "Jules", "Adam", "Louis", "Jade", "Louise", "Lola", "Emma", "Lou", "Tibo"}; 
             // Initialise une liste des noms (noms aléatoire)
             string[] ListeNoms = {"Dupont", "Martin", "Inshape", "Papin", "Bernard", "Robert", "Leroy", "Lefèvre", "Millot", "Girard", "Moreau", "Simon", "Durand", "Dubois"}; 
+            // Initialise une liste d'adresse (adresses aléatoire)
+            string[] ListeAdresse = { "12 Rue de la République, 75001 Paris", "45 Avenue Jean Jaurès, 31000 Toulouse", "78 Boulevard de la Liberté, 69003 Lyon", "33 Rue des Fleurs, 13006 Marseille", "15 Place de la Comédie, 34000 Montpellier", "22 Rue du Commerce, 44000 Nantes", "56 Avenue des Champs-Élysées, 75008 Paris", "9 Rue de la Gare, 67000 Strasbourg", "101 Boulevard de la Mer, 06200 Nice", "8 Rue du Marché, 59800 Lille" };
             string prenom = ListePrenoms[random.Next(ListePrenoms.Length)]; // Prend un prénoms aléatoire dans la liste des prénoms
             string nom = ListeNoms[random.Next(ListeNoms.Length)];// Prend un nom aléatoire dans la liste des nom
-            Jouet jouet = AgeToJouet(random.Next(18)); // Prend un âge aléatoire entre 0 et 18 ans et le transforme en jouet 
-            Lettre lettreAléatoire = new Lettre(jouet,nom, prenom); // Créer la lettre avec les valeurs aléatoires plus hauts
+            Continents continent = RandomContinent(random.Next(5));// Prend un nombre aléatoire et en fait un Continent
+            Jouet jouet = AgeToJouet(random.Next(18)); // Prend un âge aléatoire entre 0 et 18 ans et le transforme en jouet
+            string adresse = ListeAdresse[random.Next(ListeAdresse.Length)];// Prend une adresse aléatoire dans la liste des adresses
+            Lettre lettreAléatoire = new Lettre(jouet,nom, prenom, continent, adresse); // Créer la lettre avec les valeurs aléatoires plus hauts
             //Ajoute la lettre à la pile 
             PileDeLettre.Empile(lettreAléatoire);
         }
@@ -413,7 +648,7 @@ namespace ProjectNoel
             Console.WriteLine("-------------Tests classe Lettres");
 
             //Création 
-            Lettre lettreTest = new Lettre(Jouet.Tricycle, "Robert", "LEROI"); 
+            Lettre lettreTest = new Lettre(Jouet.Tricycle, "Robert", "LEROI", Continents.Afrique, "8 rue Charle De Gaule"); 
 
             //Affichage
             lettreTest.Affiche();
@@ -428,4 +663,3 @@ namespace ProjectNoel
     }
 
 }
-
